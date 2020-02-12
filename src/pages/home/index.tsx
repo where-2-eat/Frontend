46add705bbe6593f502c86bf21ebf19a5ae54003
AnssamGhezala <anssam.ghezala@mail.mcgill.ca>
2020-02-11 22:47:02 -0500

    Solves #3

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
-import GoogleMap from "../../components/map";
-import { Place, MapView } from "../../components/map/util/types";
-import { getUserPosition } from "../../components/map/util";
+import React from "react";
 import { ConfigType } from "../../config";
 import { User } from "../../util/types";
+import './Home.css';
+// import GoogleMap from "../../components/map";
+// import { Place, MapView } from "../../components/map/util/types";
+// import { getUserPosition } from "../../components/map/util";
 
 type HomeProps = {
   context: ConfigType;
@@ -12,48 +13,56 @@ type HomeProps = {
 };
 
 const Home: React.FC<HomeProps> = ({ context, user, handleOnLogout }) => {
-  const { defaultMapView, googleApiKey } = context;
+  // TODO: the following is to be used once we set up Google Map API properly
+  // const { defaultMapView, googleApiKey } = context;
 
-  const [mapView, setMapView] = useState<MapView>(defaultMapView);
-  const [geolocated, setGeolocated] = useState<boolean>(false);
+  // const [mapView, setMapView] = useState<MapView>(defaultMapView);
+  // const [geolocated, setGeolocated] = useState<boolean>(false);
 
-  const [places, setPlaces] = useState<Place[]>([]);
-  const [selectedPlace, setSelectedPlace] = useState<Place>();
+  // const [places, setPlaces] = useState<Place[]>([]);
+  // const [selectedPlace, setSelectedPlace] = useState<Place>();
 
-  if (!geolocated) {
-    getUserPosition()
-      .then(position => {
-        const { latitude, longitude } = position.coords;
-        setMapView({
-          zoom: mapView.zoom,
-          lat: latitude,
-          lng: longitude
-        });
-      })
-      .catch(console.log)
-      .finally(() => {
-        setGeolocated(true);
-      });
-  }
+  // if (!geolocated) {
+  //   getUserPosition()
+  //     .then(position => {
+  //       const { latitude, longitude } = position.coords;
+  //       setMapView({
+  //         zoom: mapView.zoom,
+  //         lat: latitude,
+  //         lng: longitude
+  //       });
+  //     })
+  //     .catch(console.log)
+  //     .finally(() => {
+  //       setGeolocated(true);
+  //     });
+  // }
 
-  const handleOnPlaceClick = (place: Place) => {
-    setSelectedPlace(place);
-  };
+  // const handleOnPlaceClick = (place: Place) => {
+  //   setSelectedPlace(place);
+  // };
 
-  const handleOnMapChange = (newMapView: MapView) => {
-    setMapView({ ...newMapView });
-  };
+  // const handleOnMapChange = (newMapView: MapView) => {
+  //   setMapView({ ...newMapView });
+  // };
 
   return (
-    <GoogleMap
-      apiKey={googleApiKey}
-      mapView={mapView}
-      defaultView={defaultMapView}
-      places={places}
-      selectedPlace={selectedPlace}
-      handleOnPlaceClick={handleOnPlaceClick}
-      onMapChange={handleOnMapChange}
-    />
+    <div className="Home">
+      <div className="wrapper">
+        <div className="group-image image" />
+        <div className="solo-image image" />
+      </div>
+    </div>
+
+    // <GoogleMap
+    //   apiKey={googleApiKey}
+    //   mapView={mapView}
+    //   defaultView={defaultMapView}
+    //   places={places}
+    //   selectedPlace={selectedPlace}
+    //   handleOnPlaceClick={handleOnPlaceClick}
+    //   onMapChange={handleOnMapChange}
+    // />
   );
 };
 
