f006dcc68e98f76c52d010430ef553710ce85971
Anssam GHEZALA <agheza1@lab9-5.CS.McGill.CA>
2020-02-17 05:35:32 -0500

    fix import statements

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { ConfigType } from "../../config";
 import { User } from "../../util/types";
 import './Home.css';
+import { Link } from "react-router-dom";
 
 type HomeProps = {
   context: ConfigType;
