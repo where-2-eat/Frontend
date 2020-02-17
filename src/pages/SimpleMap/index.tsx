dbd08791d5d977359dadf34ac38b5424b2eb820b
AnssamGhezala <anssam.ghezala@mail.mcgill.ca>
2020-02-17 01:37:50 -0500

    add pin drop on click of map

--- src/pages/SimpleMap/index.tsx
+++ src/pages/SimpleMap/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import GoogleMapReact from "google-map-react";
 import Marker from "./Maker";
 
@@ -6,24 +6,27 @@ const SimpleMap = (props: any) => {
   const [center, setCenter] = useState({ lat: 11.0168, lng: 76.9558 });
   const [zoom, setZoom] = useState(11);
   const [marker, setMarker] = useState({ lat: 11.0168, lng: 76.9558 });
-  const _setMarker = ({
-    x,
-    y,
-    lat,
-    lng,
-    event
-  }: {
-    x: any;
-    y: any;
-    lat: any;
-    lng: any;
-    event: any;
-  }) => {
-    console.log(x, y, lat, lng, event);
-    setMarker({lat: lat, lng: 76.9558});
-  };
+  const [position, setPosition] = useState({});
+  const [error, setError] = useState('');
 
-  const createMarker = () => {};
+    const onChange = ({coords}: {coords: any}) => {
+      setPosition({
+        latitude: coords.latitude,
+        longitude: coords.longitude,
+      });
+    };
+    
+    const onError = (error: any) => {
+      setError(error.message);
+    };
+
+    useEffect(() => {
+      
+    }, []);
+
+  const _setMarker = ({ lat, lng  }: { lat: number; lng: number; }) => {
+    setMarker({lat: lat, lng: lng});
+  };
 
   return (
     <div style={{ height: "100vh", width: "100%" }}>
@@ -31,11 +34,11 @@ const SimpleMap = (props: any) => {
         bootstrapURLKeys={{ key: "AIzaSyAZSDHMCOwasni6ozoypX_VLm4WmoXYndM" }}
         defaultCenter={center}
         defaultZoom={zoom}
-        onClick={_onClick}
+        onClick={_setMarker}
       >
         <Marker
-          lat={11.0168}
-          lng={76.9558}
+          lat={marker.lat}
+          lng={marker.lng}
           name="My Marker"
           color="blue"
           draggable="true"
